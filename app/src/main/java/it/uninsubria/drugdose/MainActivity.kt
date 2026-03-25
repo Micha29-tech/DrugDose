781a070c57e235301698d30480ccb1c51c339d7e
Micha29-tech <michaelascafdi@gmail.com>
2026-03-26 00:51:42 +0100

    Aggiunta layout XML base e collegamento MainActivity - aggiunti primi componenti UI (TextView,EditText) - collegato layout a MainActivity con setContentView - inizializzato findViewById per input utente

--- app/src/main/java/it/uninsubria/drugdose/MainActivity.kt
+++ app/src/main/java/it/uninsubria/drugdose/MainActivity.kt
@@ -1,10 +1,26 @@
 package it.uninsubria.drugdose
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etPeso = findViewById<EditText>(R.id.etPeso)
+        val etAltezza = findViewById<EditText>(R.id.etAltezza)
+        val etEta = findViewById<EditText>(R.id.etEta)
+        val btnCalcola = findViewById<EditText>(R.id.btnCalcola)
+        val txtRisultato = findViewById<EditText>(R.id.txtRisultato)
+
+        btnCalcola.setOnClickListener {
+            val peso = etPeso.text.toString()
+            val altezza = etAltezza.text.toString()
+            val eta = etEta.text.toString()
+            txtRisultato = "Peso: $peso kg, Altezza: $altezza cm, Età: $eta"
+        }
     }
 }
