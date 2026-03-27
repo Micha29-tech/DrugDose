6c5c9a5618b6e99271bdf3872278079946897313
Micha29-tech <michaelascafdi@gmail.com>
2026-03-27 14:24:43 +0100

    Completata UI base e logina in MainActivity.kt - completato layout activity_main con campi input (peso,altezza,eta) e bottone calcolo - collegato layout a MainActivity con setContentView - gestione click e output risultato

--- app/src/main/java/it/uninsubria/drugdose/MainActivity.kt
+++ app/src/main/java/it/uninsubria/drugdose/MainActivity.kt
@@ -1,7 +1,9 @@
 package it.uninsubria.drugdose
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -13,14 +15,46 @@ class MainActivity : AppCompatActivity() {
         val etPeso = findViewById<EditText>(R.id.etPeso)
         val etAltezza = findViewById<EditText>(R.id.etAltezza)
         val etEta = findViewById<EditText>(R.id.etEta)
-        val btnCalcola = findViewById<EditText>(R.id.btnCalcola)
-        val txtRisultato = findViewById<EditText>(R.id.txtRisultato)
+        val btnCalcola = findViewById<Button>(R.id.btnCalcola)
+        val txtRisultato = findViewById<TextView>(R.id.txtRisultato)
 
         btnCalcola.setOnClickListener {
-            val peso = etPeso.text.toString()
-            val altezza = etAltezza.text.toString()
-            val eta = etEta.text.toString()
-            txtRisultato = "Peso: $peso kg, Altezza: $altezza cm, Età: $eta"
+            val pesoText = etPeso.text.toString().trim()
+            val altezzaText = etAltezza.text.toString().trim()
+            val etaText = etEta.text.toString().trim()
+
+            if(pesoText.isEmpty()){
+                etPeso.error="Inserire il peso"
+                return@setOnClickListener
+            }
+            if(altezzaText.isEmpty()){
+                etAltezza.error="Inserire l'altezza"
+                    return@setOnClickListener
+                }
+
+            if(etaText.isEmpty()){
+                etEta.error="Inserire l'età"
+                return@setOnClickListener
+            }
+
+            val peso=pesoText.toDoubleOrNull()
+            val altezza= altezzaText.toDoubleOrNull()
+            val eta= etaText.toIntOrNull()
+
+                if(peso == null || peso <= 0 ){
+                    etPeso.error = "Peso non valido"
+                    return@setOnClickListener
+
+                }
+                if(altezza == null || altezza <= 0 ){
+                    etAltezza.error="Altezza non valida"
+                    return@setOnClickListener
+                }
+                 if(eta == null || eta < 0){
+                     etEta.error = "Età non valida"
+                     return@setOnClickListener
+            }
+            txtRisultato.text = "Peso: $peso kg, Altezza: $altezza cm, Età: $eta"
         }
     }
 }
